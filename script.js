b05231779fe2d65a5587aa66665f6825a97cdcfa
Avaz <avazbekrozimboyev08@gmail.com>
2024-11-30 12:38:23 +0500

    figma va js

---
+++ script.js
@@ -0,0 +1,35 @@
+//1-masala
+// let soz = "Salom dunyo";
+// for(let i=soz.length-1; i>=0; i--){
+//     console.log(soz[i]);
+// }
+
+//2-masala
+
+// let num = "level";
+// let res = "";
+
+// for (let i = num.length - 1; i>=0; i--) {
+//   res += num[i];
+// }
+// if(num==res){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+//3-masala
+
+// let soz = "hLjshJsK";
+// let count = 0;
+
+// for (let i = 0; i <= soz.length-1; i++) {
+// if(soz[i] == soz[i].toUpperCase()){
+//     count++;
+// }
+// }
+// console.log(count);
+
+
+
+//4-masala
